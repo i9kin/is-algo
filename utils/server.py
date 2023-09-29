3449144b786f26d1152c65b8be74897deaa0571e
9kin <gate.web.tester@mail.ru>
2023-09-29 13:40:13 +0000

    deploy: fd407683c654f12cd14c467a78065100889d9630

---
+++ utils/server.py
@@ -0,0 +1,26 @@
+# run=uvicorn server:app --host 0.0.0.0
+from fastapi import FastAPI, HTTPException
+from os import system
+import json
+
+app = FastAPI()
+PYTHON = "python3"
+
+try:
+    with open("secrets.json", "r") as f:
+        config_json = json.load(f)
+        USER = config_json["USER"]
+        REPO = config_json["REPO"]
+        GH_PASSWORD = config_json["GH_PASSWORD"]
+        CICD_HASH =  config_json["CICDHASH"]
+except:
+    print("error while reading secrets.json")
+    exit(0)
+
+
+@app.get("/cicd/{hash}")
+def pull(hash: str):
+    if hash != CICD_HASH:
+        raise HTTPException(status_code=404, detail="bad")
+    system(f"{PYTHON} cicd.py &")
+    return "ok"
