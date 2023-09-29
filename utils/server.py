841d3aa984012e464298bdbf7c65fdcdef4e7ccd
9kin <gate.web.tester@mail.ru>
2023-09-29 16:29:46 +0300

    self-hosted ci/cd fix

--- utils/server.py
+++ utils/server.py
@@ -1,12 +1,26 @@
+# run=uvicorn server:app --host 0.0.0.0
 from fastapi import FastAPI, HTTPException
 from os import system
+import json
 
 app = FastAPI()
-PYTHON = 'python3'
+PYTHON = "python3"
 
-@app.get("/cicd/{HASH}")
+try:
+    with open("secrets.json", "r") as f:
+        config_json = json.load(f)
+        USER = config_json["USER"]
+        REPO = config_json["REPO"]
+        GH_PASSWORD = config_json["GH_PASSWORD"]
+        CICD_HASH =  config_json["CICDHASH"]
+except:
+    print("error while reading secrets.json")
+    exit(0)
+
+
+@app.get("/cicd/{hash}")
 def pull(hash: str):
-    if not validate(owner, repo, hash):
-        raise HTTPException(status_code=404, detail='(')
-    system(f'{PYTHON} cicd.py &')
+    if hash != CICD_HASH:
+        raise HTTPException(status_code=404, detail="bad")
+    system(f"{PYTHON} cicd.py &")
     return "ok"
