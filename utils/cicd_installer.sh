b250117a6b10aa364772534facdb08386ad1830e
9kin <gate.web.tester@mail.ru>
2023-11-02 12:00:32 +0300

    Fixed cicd installer

--- utils/cicd_installer.sh
+++ utils/cicd_installer.sh
@@ -2,4 +2,6 @@ curl https://sh.rustup.rs -sSf | sh
 source "$HOME/.cargo/env"
 sh book_requirements.sh
 pip3 install -r cicd_requirements.txt
-sudo apt install texlive-latex-extra pdf2svg
+sudo apt install pdf2svg
+sudo apt install texlive-latex-extra texlive-luatex
+luaotfload-tool -v -vvv -u
